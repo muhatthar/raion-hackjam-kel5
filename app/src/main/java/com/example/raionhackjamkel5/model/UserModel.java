42029c65bffc5b268e4a8891069443ee064e6662
MuhAtthar <muhatthar@student.ub.ac.id>
2023-08-28 04:09:13 +0700

    signup login

---
+++ app/src/main/java/com/example/raionhackjamkel5/model/UserModel.java
@@ -0,0 +1,41 @@
+package com.example.raionhackjamkel5.model;
+
+public class UserModel {
+    private String nama;
+    private String whatsapp;
+    private String asalKota;
+
+    public UserModel(){
+
+    }
+
+    public UserModel(String nama, String whatsapp, String asalKota) {
+        this.nama = nama;
+        this.whatsapp = whatsapp;
+        this.asalKota = asalKota;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(String whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
+    public String getAsalKota() {
+        return asalKota;
+    }
+
+    public void setAsalKota(String asalKota) {
+        this.asalKota = asalKota;
+    }
+}
