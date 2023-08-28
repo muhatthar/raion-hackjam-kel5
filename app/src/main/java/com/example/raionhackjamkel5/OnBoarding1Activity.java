3a727930603134ad57ea72ae5dbda0f6b1ee279b
MuhAtthar <muhatthar@student.ub.ac.id>
2023-08-29 02:40:59 +0700

    on board

---
+++ app/src/main/java/com/example/raionhackjamkel5/OnBoarding1Activity.java
@@ -0,0 +1,14 @@
+package com.example.raionhackjamkel5.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoarding1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding1);
+    }
+}
