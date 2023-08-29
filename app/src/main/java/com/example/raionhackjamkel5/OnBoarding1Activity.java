1a8eff0c19e0e9f7d8639b41498eba109f22a833
MuhAtthar <muhatthar@student.ub.ac.id>
2023-08-29 12:30:31 +0700

    on boarding layout

--- app/src/main/java/com/example/raionhackjamkel5/OnBoarding1Activity.java
+++ app/src/main/java/com/example/raionhackjamkel5/OnBoarding1Activity.java
@@ -1,14 +1,30 @@
-package com.example.raionhackjamkel5.homepage;
+package com.example.raionhackjamkel5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.raionhackjamkel5.R;
 
 public class OnBoarding1Activity extends AppCompatActivity {
+
+    AppCompatButton btn_Next;
+    LinearLayout dots_Layout;
+    ViewPager view_Pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding1);
+
+        btn_Next = findViewById(R.id.btnNext);
+        dots_Layout = findViewById(R.id.dotsLayout);
+        view_Pager = findViewById(R.id.slider);
+
+        OnBoardingAdapter adapter = new OnBoardingAdapter(this);
+        view_Pager.setAdapter(adapter);
     }
 }
