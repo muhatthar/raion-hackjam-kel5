eff41655f03bad57fb51b522b7011723408ab942
MuhAtthar <muhatthar@student.ub.ac.id>
2023-08-29 16:36:21 +0700

    dots

--- app/src/main/java/com/example/raionhackjamkel5/OnBoarding1Activity.java
+++ app/src/main/java/com/example/raionhackjamkel5/OnBoarding1Activity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.raionhackjamkel5.R;
 
@@ -14,6 +18,8 @@ public class OnBoarding1Activity extends AppCompatActivity {
     AppCompatButton btn_Next;
     LinearLayout dots_Layout;
     ViewPager view_Pager;
+    TextView[] dots;
+    int currentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +32,30 @@ public class OnBoarding1Activity extends AppCompatActivity {
 
         OnBoardingAdapter adapter = new OnBoardingAdapter(this);
         view_Pager.setAdapter(adapter);
+        btn_Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view_Pager.setCurrentItem(currentPosition + 1, true);
+            }
+        });
+    }
+
+    private void dotsFunction(int pos){
+        dots = new TextView[2];
+        dots_Layout.removeAllViews();
+
+        for (int i = 0; i < dots.length; i++){
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("-"));
+            dots[i].setTextColor(Color.parseColor("#E9E9E9"));
+            dots[i].setTextSize(30);
+
+            dots_Layout.addView(dots[i]);
+        }
+
+        if (dots.length > 0){
+            dots[pos].setTextColor(Color.parseColor("#3C8918"));
+            dots[pos].setTextSize(40);
+        }
     }
 }
